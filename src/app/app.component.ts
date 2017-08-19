030f6c095f2f82ec7c9cf248340653ed2bebb466
jimlapine <jim.lapine@gmail.com>
2017-08-19 17:59:21 -0400

    Minor Cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, NgForm, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
